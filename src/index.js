f18c3b0a91020550b02e2c1fde57fb186b492e08
Timur Gafiulin <nem00700@gmail.com>
2017-11-07 21:31:54 +0300

    delete extra code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import tasks from './reducer/tasks';
 import reducer from './reducer/index';
 
 const store = createStore(reducer);
