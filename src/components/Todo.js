6b9dbbbd8b6eeee5a5b40237eddeb19d96fb219e
Timur Gafiulin <nem00700@gmail.com>
2017-11-13 22:02:58 +0300

    add button for create new todolist

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,23 @@ import TodoList from './TodoList';
 
 
 export default class Todo extends Component{
+    state = {
+        quantity: 0
+    }
 
+    addTodoList(){
+        this.setState({quantity: this.state.quantity + 1});
+    }
 
     render(){
+        const todolist = [];
+        for(let i = 0; i < this.state.quantity; i++){
+            todolist.push(<TodoList id={i} key={i} />);
+        }
+
         return <div>
-            <TodoList id='1' />
-            <TodoList id='2' />
+            {todolist}
+            <button onClick={this.addTodoList.bind(this)}>Add TodoList</button>
         </div>;
     }
 }
